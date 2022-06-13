0baa10e386ad45e47f2e0eb047d8611ec8869a96
Sushil <107347115+sus0001@users.noreply.github.com>
2022-06-13 14:57:47 +0545

    Update main.py

--- main.py
+++ main.py
@@ -8,78 +8,80 @@ app = FastAPI()
 data_kMarket_en = kalimati_market_en()
 
 
+def check_lang(langs, value1, value2):
+    # langs = ""
+    if langs == 'en':
+        return value1
+    elif langs == 'np':
+        return value2
+    else:
+        return {"Details": "Not found"}
+
 
 @app.get("/kalimati_market/{lang}")
-def market_now(lang: str): 
-    if lang == 'np':               
-        return {date_header_np(): {
+def market_now(lang: str):
+    return check_lang(lang,     
+        {date_header_np(): {
                         "वस्तु": kalimati_market_np(0),
                         "एकाइ": kalimati_market_np(1),
                         "न्यूनतम": kalimati_market_np(2),
                         "अधिकतम": kalimati_market_np(3),
                         "औसत": kalimati_market_np(4)
-                }}
+                }
+                }, 
 
-    elif lang == 'en':
-        return {date_header_en(): {
+        {date_header_en(): {
                 "Commodity": data_kMarket_en[0],
                 "Unit": data_kMarket_en[1],
                 "Minimum": data_kMarket_en[2],
                 "Maximum": data_kMarket_en[3],
                 "Average": data_kMarket_en[4]
-
-            }}
+            }
+            }
+            )    
     
-    else:
-        return {"Details": "Not found"}
-            
 
 @app.get("/kalimati_market/{lang}/commodity")
 def commodity(lang: str):
-    if lang == 'en':
-        return {date_header_en(): {"Commodity": data_kMarket_en[0]}}
-    elif lang == 'np':
-        return {date_header_np(): {"वस्तु": kalimati_market_np(0)}}
-    else:
-        return {"Details": "Not found"}
-
+    return check_lang(lang, {
+        date_header_en(): {
+            "Commodity": data_kMarket_en[0]}}, 
+        {date_header_np():{
+            "वस्तु": kalimati_market_np(0)}})
+    
 
 @app.get("/kalimati_market/{lang}/unit")
 def unit(lang: str):
-    if lang == 'en':
-        return {date_header_en(): {"Unit": data_kMarket_en[1]}}
-    elif lang == 'np':
-        return {date_header_np(): {"एकाइ": kalimati_market_np(1)}}
-    else:
-        return {"Details": "Not found"}
+    return check_lang(lang, {
+        date_header_en(): {
+            "Commodity": data_kMarket_en[1]}}, 
+        {date_header_np():{
+            "वस्तु": kalimati_market_np(1)}})
 
 
 @app.get("/kalimati_market/{lang}/minimum")
 def minimum(lang: str):
-    if lang == 'en':
-        return {date_header_en(): {"Minimum": data_kMarket_en[2]}}
-    elif lang == 'np':
-        return {date_header_np(): {"्यूनतम": kalimati_market_np(2)}}
-    else:
-        return {"Details": "Not found"}
+    return check_lang(lang, {
+        date_header_en(): {
+            "Commodity": data_kMarket_en[2]}}, 
+        {date_header_np():{
+            "वस्तु": kalimati_market_np(2)}})
 
 
 @app.get("/kalimati_market/{lang}/maximum")
 def maximum(lang: str):
-    if lang == 'en':
-        return {date_header_en(): {"Maximum": data_kMarket_en[3]}}
-    elif lang == 'np':
-        return {date_header_np(): {"अधिकतम": kalimati_market_np(3)}}
-    else:
-        return {"Details": "Not found"}
+    return check_lang(lang, {
+        date_header_en(): {
+            "Commodity": data_kMarket_en[3]}}, 
+        {date_header_np():{
+            "वस्तु": kalimati_market_np(3)}})
 
 
 @app.get("/kalimati_market/{lang}/average")
 def average(lang: str):
-    if lang == 'en':
-        return {date_header_en(): {"Average": data_kMarket_en[4]}}
-    elif lang == 'np':
-        return {date_header_np(): {"औसत": kalimati_market_np(4)}}
-    else:
-        return {"Details": "Not found"}
+    return check_lang(lang, {
+        date_header_en(): {
+            "Commodity": data_kMarket_en[4]}}, 
+        {date_header_np():{
+            "वस्तु": kalimati_market_np(4)}})
 
