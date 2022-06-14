0ca9a2a632f3d5df0a1d2420c9e1b2939b68b4cb
Sushil <107347115+sus0001@users.noreply.github.com>
2022-06-14 18:11:09 +0545

    Update functionalities_selenium.py

--- kalimati_market/functionalities_selenium.py
+++ kalimati_market/functionalities_selenium.py
@@ -5,10 +5,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from selenium.common.exceptions import StaleElementReferenceException
 from .functionalities_bs4 import get_ua
 import random
 import os
